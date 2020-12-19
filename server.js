89ac8533c1e33bda2118c9184884ee6a7405f66f
Truc Nguyen <trucn0215@gmail.com>
2020-12-18 23:30:55 -0800

    fs.readfile from the database and turn it into JSOn

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require("express");
 const path = require("path");
 
 const fs = require("fs");
-const { dirname } = require("path");
+const dbNotePath = require("./db/db.json");
+const { json } = require("express");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -12,56 +13,68 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+// API rountes
+// GET - read the db.json file and return all saved notes as JSON
 app.get("/api/notes", function (req, res) {
 
     // Use the fs module to read the file
-
-    // THEN parse the file contects with JSON.parse() to get the real data.
-
-    // Send the parsed data back to client with res.send()
-
+    fs.readFile(dbNotePath, (err, data) => {
+        if (err) {
+            console.error(err);
+        }
+        const readData = JSON.parse(data)
+        console.log(readData);
+        return res.json(readData);
+    })
 })
 
+// POST - receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client.
 app.post("/api/notes", function (req, res) {
 
     // Access the POSTed data in `req.body`
+    const newNoteData = req.body;
 
-    // Use the fs module to read the file
+    // Use the fs module to write the file
+    fs.writeFile(path.join(__dirname, dbNote), newNoteData)
 
     // THEN parse the file contects with JSON.parse() to get the real data.
 
     // PUSH the `req.body` to the array list.
+    dbNote.push(newNoteData);
 
     //JSON.stringify() the array list back into a JSON string
 
     // THEN save the contects back to the `db.json` with the "fs" module
 
+    console.log(dbNote);
 })
 
-app.delete("/api/notes/:id", function(req, res) {
-        
-    // Access :id from `req.params.id`
 
-    // Use the fs module to read the file
+// DELETE - receive a query parameter containing ID of a note to delete.
+// app.delete("/api/notes/:id", function(req, res) {
 
-    // THEN parse the file contects with JSON.parse() to get the real data.
+// Access :id from `req.params.id`
 
-    // OPTION A
-        // Find the matching index using findIndex()
+// Use the fs module to read the file
 
-        // Remove thetarget element using .splice()
+// THEN parse the file contects with JSON.parse() to get the real data.
 
-    // OPTION B
-        // Use the array.filter() method to filter out the matching element
-        // myArray = myArray.filter( element => element.id !== req.params.id);
+// OPTION A
+// Find the matching index using findIndex()
 
-    // Return any time of success message.
+// Remove thetarget element using .splice()
 
-})
+// OPTION B
+// Use the array.filter() method to filter out the matching element
+// myArray = myArray.filter( element => element.id !== req.params.id);
+
+// Return any time of success message.
+
+// })
 
 app.get("/notes", function (req, res) {
 
-    // return the contents of the index.html
+    // return the contents of the notes.html
     res.sendFile(path.join(__dirname, "public/notes.html"))
 })
 
